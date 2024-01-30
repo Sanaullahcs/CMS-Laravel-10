de0e1976135725bd225d18104c8557fcab19c816
Sana Ullah <sanaullahbeha@gmail.com>
2024-01-30 17:39:41 +0500

    project setup

---
+++ resources/views/admin/blogs.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/admin/blogs.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <h1>Blogs</h1>
+    <!-- Blogs specific content goes here -->
+@endsection
---
+++ resources/views/admin/services.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/admin/services.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <h1>Services</h1>
+    <!-- Services specific content goes here -->
+@endsection
